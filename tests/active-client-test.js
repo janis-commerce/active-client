9fe796c084ea20419469ea3092b06de5d7458084
Juan <juan@fizzmod.com>
2019-06-25 18:45:10 -0300

    Model client + active client get

--- tests/active-client-test.js
+++ tests/active-client-test.js
@@ -1,13 +1,34 @@
 'use strict';
 
+const path = require('path');
 const assert = require('assert');
+const mockRequire = require('mock-require');
 
 const sandbox = require('sinon').createSandbox();
 
-const { ActiveClient, ActiveClientError } = require('./..');
+const ActiveClient = require('../');
+const { ActiveClientError } = require('../lib');
+
+/* eslint-disable prefer-arrow-callback */
 
 describe('ActiveClient', () => {
 
-	// your tests here...
+	/*
+	before(() => {
+		mockRequire(path.join(process.env(), 'config', 'database.json'), {
+
+		});
+	});
+
+	after(() => {
+		mockRequire.stopAll();
+	});
+*/
+
+	it('something', async function() {
+
+		await ActiveClient.getClient();
+
+	});
 
 });
